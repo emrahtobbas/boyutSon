293037d70df430d97481be09ccf1e2e0e9aaabcd
emrah tobbas <emrahtobbas@gmail.com>
2022-02-13 13:26:47 +0300

    v1

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1644747939,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"c2webappstart.js",
+		"images/soruspt-sheet0.png",
+		"images/soruspt-sheet1.png",
+		"images/seceneka-sheet0.png",
+		"images/secenekb-sheet0.png",
+		"images/secenekc-sheet0.png",
+		"images/secenekd-sheet0.png",
+		"images/bilgispt-sheet0.png",
+		"images/sonucspt-sheet0.png",
+		"images/arkaplan-sheet0.png",
+		"images/arkaplan-sheet1.png",
+		"images/devamspt-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"sorular.txt"
+	]
+}
